c86598aa3dc03c9d441b67b1f2f1d0b975f8386e
li-maksim <max.mail4@yandex.ru>
2024-09-17 22:10:10 +0300

    Added function to check the game's winner

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -5,7 +5,11 @@ const Gameboard = (() => {
     const gameboard = [];
     for (i = 0; i < 9; i++) {
         const sector = document.createElement('div');
+        function addMark() {
+            sector.textContent = 'X';
+        };
         sector.setAttribute('class', 'sector');
+        sector.addEventListener('click', addMark);
         board.appendChild(sector);
         const sectorID = {mark: ''};
         gameboard.push(sectorID);
@@ -25,7 +29,7 @@ const Gameboard = (() => {
 const Player1 = {
     name: 'Player 1',
     score: 0,
-    playerMark: 'x';
+    playerMark: 'x',
     changeName(newName) {
         this.name = newName;
     },
@@ -39,7 +43,7 @@ const Player1 = {
 const Player2 = {
     name: 'Player 2',
     score: 0,
-    playerMark: 'o';
+    playerMark: 'o',
     changeName(newName) {
         this.name = newName;
     },
@@ -51,7 +55,68 @@ const Player2 = {
 };
 
 const GameLogic = (() => {
+    const markX = Player1.playerMark;
+    const markO = Player2.playerMark;
+    function checkWinner() {
+        if ((Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[1].mark,
+            Gameboard.gameboard[2].mark == markX) ||
+            (Gameboard.gameboard[3].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[5].mark == markX) ||
+            (Gameboard.gameboard[6].mark,
+            Gameboard.gameboard[7].mark,
+            Gameboard.gameboard[8].mark == markX) ||
+            (Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[3].mark,
+            Gameboard.gameboard[6].mark == markX) ||
+            (Gameboard.gameboard[1].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[7].mark == markX) ||
+            (Gameboard.gameboard[2].mark,
+            Gameboard.gameboard[5].mark,
+            Gameboard.gameboard[8].mark == markX) ||
+            (Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[8].mark == markX) ||
+            (Gameboard.gameboard[2].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[6].mark == markX)
+        ) {
+            alert(`${Player1.name} wins!`);
+        } else if (
+            Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[1].mark,
+            Gameboard.gameboard[2].mark == markO ||
+            Gameboard.gameboard[3].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[5].mark == markO ||
+            Gameboard.gameboard[6].mark,
+            Gameboard.gameboard[7].mark,
+            Gameboard.gameboard[8].mark == markO ||
+            Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[3].mark,
+            Gameboard.gameboard[6].mark == markO ||
+            Gameboard.gameboard[1].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[7].mark == markO ||
+            Gameboard.gameboard[2].mark,
+            Gameboard.gameboard[5].mark,
+            Gameboard.gameboard[8].mark == markO ||
+            Gameboard.gameboard[0].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[8].mark == markO ||
+            Gameboard.gameboard[2].mark,
+            Gameboard.gameboard[4].mark,
+            Gameboard.gameboard[6].mark == markO
+        ) {
+            alert((`${Player2.name} wins!`))
+        } else {
+            alert('Tie!');
+        }
+    };
 
+    return {checkWinner};
 })();
 
 function showGB() {
