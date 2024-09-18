0d6293fca352592a0ef2cfb4648dbddb4905ef21
li-maksim <max.mail4@yandex.ru>
2024-09-18 20:13:16 +0300

    Small refactoring
    Moved all the display/DOM logic to the Display
    object.

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,50 +1,11 @@
 const Gameboard = (() => {
-    const body = document.querySelector('body');
-    const board = document.createElement('div');
-    board.setAttribute('class', 'board');
     const gameboard = [];
     for (i = 0; i < 9; i++) {
-        const sector = document.createElement('div');
-        sector.setAttribute('class', 'sector');
-        board.appendChild(sector);
         const sectorID = {mark: ''};
         gameboard.push(sectorID);
     };
 
-    function changeMark(num, val) {
-        if (val == 'x' && gameboard[num].mark == '') {
-            gameboard[num].mark = 'x';
-        } else  if (val == 'o' && gameboard[num].mark == '') {
-            gameboard[num].mark = 'o';
-        };
-    };
-    body.appendChild(board);
-    const sectors = document.querySelectorAll('.sector');
-    sectors.forEach(function(btn, idx) {
-        function addMark() {
-            if (GameLogic.checkTurns() == true && sectors[idx].textContent == '') {
-                changeMark(idx, 'x');
-                btn.textContent = 'x';
-                GameLogic.switchTurns();
-                GameLogic.checkWinner();
-            } else if (GameLogic.checkTurns() == false && sectors[idx].textContent == '') {
-                changeMark(idx, 'o');
-                btn.textContent = 'o';
-                GameLogic.switchTurns();
-                GameLogic.checkWinner();
-            }
-        };
-        btn.addEventListener('click', addMark);
-    });
-
-    const restartFn = () => {GameLogic.restart()};
-    const restartBtn = document.createElement('button');
-    restartBtn.setAttribute('class', 'restart_btn');
-    restartBtn.textContent = 'restart';
-    restartBtn.addEventListener('click', restartFn);
-    body.appendChild(restartBtn);
-
-    return {gameboard, sectors, restartBtn}
+    return {gameboard}
 })();
 
 const Player1 = {
@@ -162,12 +123,60 @@ const GameLogic = (() => {
         Gameboard.gameboard.forEach(function(item) {
             item.mark = '';
         });
-        Gameboard.sectors.forEach(function(item) {
+        Display.sectors.forEach(function(item) {
             item.textContent = '';
         });
     };
 
     return {switchTurns, checkTurns, checkWinner, restart};
+})();
+
+const Display = (() => {
+    const body = document.querySelector('body');
+    const board = document.createElement('div');
+    board.setAttribute('class', 'board');
+    for (i = 0; i < 9; i++) {
+        const sector = document.createElement('div');
+        sector.setAttribute('class', 'sector');
+        board.appendChild(sector);
+    };
+
+    function changeMark(num, val) {
+        if (val == 'x' && Gameboard.gameboard[num].mark == '') {
+            Gameboard.gameboard[num].mark = 'x';
+        } else  if (val == 'o' && Gameboard.gameboard[num].mark == '') {
+            Gameboard.gameboard[num].mark = 'o';
+        };
+    };
+
+    body.appendChild(board);
+    const sectors = document.querySelectorAll('.sector');
+    sectors.forEach(function(btn, idx) {
+        function addMark() {
+            if (GameLogic.checkTurns() == true && sectors[idx].textContent == '') {
+                changeMark(idx, 'x');
+                btn.textContent = 'x';
+                GameLogic.switchTurns();
+                GameLogic.checkWinner();
+            } else if (GameLogic.checkTurns() == false && sectors[idx].textContent == '') {
+                changeMark(idx, 'o');
+                btn.textContent = 'o';
+                GameLogic.switchTurns();
+                GameLogic.checkWinner();
+            }
+        };
+        btn.addEventListener('click', addMark);
+    });
+
+    const restartFn = () => {GameLogic.restart()};
+    const restartBtn = document.createElement('button');
+    restartBtn.setAttribute('class', 'restart_btn');
+    restartBtn.textContent = 'restart';
+    restartBtn.addEventListener('click', restartFn);
+    body.appendChild(restartBtn);
+
+    return {sectors, restartFn}
+
 })();
 
 function showGB() {
