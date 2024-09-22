b998ec06418ad562fdcd958a3481ecc6afc39542
li-maksim <max.mail4@yandex.ru>
2024-09-22 18:04:20 +0300

    Moved functions to objects
    Refactored project to have all functions stored inside objects.

--- library/script.js
+++ library/script.js
@@ -29,117 +29,123 @@ class Book {
 const TheHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 304, false);
 const TheIdiot = new Book('The Idiot', 'F. M. Dostoevsky', 650, true);
 
-let indexNum = -1;
-
-function addBookToLibrary(book) {
-    const updLibrary = myLibrary.push(book);
-}
-
-function delFromLibrary(a) {
-    myLibrary.splice(a, 1);
-}
-
-addBookToLibrary(TheHobbit);
-addBookToLibrary(TheIdiot);
-
-const cards = document.querySelector('.cards');
-
-const createCard = function(obj) {
-    const cardItem = document.createElement('div');
-    indexNum += 1;
-    cardItem.setAttribute('class', 'card_item');
-    cardItem.setAttribute('data-index', indexNum);
-    cards.appendChild(cardItem);
-    const newUL = document.createElement('ul');
-    const titleLI = document.createElement('li');
-    titleLI.textContent = 'Title: ' + obj.title;
-    newUL.appendChild(titleLI);
-    const authorLI = document.createElement('li');
-    authorLI.textContent = 'Author: ' + obj.author;
-    newUL.appendChild(authorLI);
-    const pagesLI = document.createElement('li');
-    pagesLI.textContent = 'Pages: ' + obj.pages;
-    newUL.appendChild(pagesLI);
-    const readLI = document.createElement('li');
-    if (obj.read == true) {
-        readLI.textContent = 'Read'
-    } else {
-        readLI.textContent = 'Not read yet'
+const LibManagement = (() => {
+    function addBookToLibrary(book) {
+        const updLibrary = myLibrary.push(book);
     };
-    const toggleBtn = document.createElement('button');
-    if (obj.read == true) {
-        toggleBtn.textContent = 'Not read';
-        toggleBtn.style["background-color"] = 'grey';
-    } else {
-        toggleBtn.textContent = 'Read';
-        toggleBtn.style["background-color"] = 'white';
-    }
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'Delete Book';
-    function delBook() {
-        delFromLibrary(cardItem.dataset.index);
-        displayBooks();
+    function delFromLibrary(a) {
+        myLibrary.splice(a, 1);
     };
-    const toggle = function() {
-        obj.toggleRead();
-        displayBooks();
-    }
-    toggleBtn.addEventListener('click', toggle);
-    delBtn.addEventListener('click', delBook);
-    newUL.appendChild(readLI);
-    cardItem.appendChild(newUL);
-    cardItem.appendChild(toggleBtn);
-    cardItem.appendChild(delBtn);
-};
 
-function clearDisplay() {
-    while (cards.lastElementChild) {
-        cards.removeChild(cards.lastElementChild);
+    return {addBookToLibrary, delFromLibrary}
+})();
+
+const Display = (() => {
+    let indexNum = -1;
+
+    const cards = document.querySelector('.cards');
+    
+    const createCard = function(obj) {
+        const cardItem = document.createElement('div');
+        indexNum += 1;
+        cardItem.setAttribute('class', 'card_item');
+        cardItem.setAttribute('data-index', indexNum);
+        cards.appendChild(cardItem);
+        const newUL = document.createElement('ul');
+        const titleLI = document.createElement('li');
+        titleLI.textContent = 'Title: ' + obj.title;
+        newUL.appendChild(titleLI);
+        const authorLI = document.createElement('li');
+        authorLI.textContent = 'Author: ' + obj.author;
+        newUL.appendChild(authorLI);
+        const pagesLI = document.createElement('li');
+        pagesLI.textContent = 'Pages: ' + obj.pages;
+        newUL.appendChild(pagesLI);
+        const readLI = document.createElement('li');
+        if (obj.read == true) {
+            readLI.textContent = 'Read'
+        } else {
+            readLI.textContent = 'Not read yet'
+        };
+        const toggleBtn = document.createElement('button');
+        if (obj.read == true) {
+            toggleBtn.textContent = 'Not read';
+            toggleBtn.style["background-color"] = 'grey';
+        } else {
+            toggleBtn.textContent = 'Read';
+            toggleBtn.style["background-color"] = 'white';
+        }
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'Delete Book';
+        function delBook() {
+            LibManagement.delFromLibrary(cardItem.dataset.index);
+            displayBooks();
+        };
+        const toggle = function() {
+            obj.toggleRead();
+            displayBooks();
+        }
+        toggleBtn.addEventListener('click', toggle);
+        delBtn.addEventListener('click', delBook);
+        newUL.appendChild(readLI);
+        cardItem.appendChild(newUL);
+        cardItem.appendChild(toggleBtn);
+        cardItem.appendChild(delBtn);
     };
-    indexNum = -1;
-};
-
-function displayBooks() {
-    clearDisplay();
-    myLibrary.forEach(createCard);
-};
-
-const form = document.querySelector('dialog');
-
-const newBookBtn = document.querySelector('.new');
-newBookBtn.addEventListener('click', () => form.showModal());
-
-const inputs = [
-    document.querySelector('#title'),
-    document.querySelector('#author'),
-    document.querySelector('#pages'),
-    document.querySelector('#read')
-];
-
-const addBookBtn = document.querySelector('.add_btn');
-function addBook(evt) {
-    evt.preventDefault();
-    let input3 = '';
-    if (inputs[3].checked == true) {
-        input3 = true;
-    } else {
-        input3 = false;
+    
+    function clearDisplay() {
+        while (cards.lastElementChild) {
+            cards.removeChild(cards.lastElementChild);
+        };
+        indexNum = -1;
     };
-    if (inputs[0].value == '' ||
-        inputs[1].value == '' ||
-        inputs[0].value.length > 100 ||
-        inputs[1].value.length > 100 ||
-        inputs[2].value == ''
-    ) {
-        document.querySelectorAll('.required').forEach(x => x.setAttribute('class', 'red_outline'))
-        alert('Please fill all fields');
-    } else {
-        const newBook = new Book(inputs[0].value, inputs[1].value, parseInt(inputs[2].value), input3);
-        addBookToLibrary(newBook);
-        displayBooks();
-        form.close();
+    
+    function displayBooks() {
+        clearDisplay();
+        myLibrary.forEach(createCard);
     };
-};
-addBookBtn.addEventListener('click', addBook);
+    
+    const form = document.querySelector('dialog');
+    
+    const newBookBtn = document.querySelector('.new');
+    newBookBtn.addEventListener('click', () => form.showModal());
+    
+    const inputs = [
+        document.querySelector('#title'),
+        document.querySelector('#author'),
+        document.querySelector('#pages'),
+        document.querySelector('#read')
+    ];
+    
+    const addBookBtn = document.querySelector('.add_btn');
+    function addBook(evt) {
+        evt.preventDefault();
+        let input3 = '';
+        if (inputs[3].checked == true) {
+            input3 = true;
+        } else {
+            input3 = false;
+        };
+        if (inputs[0].value == '' ||
+            inputs[1].value == '' ||
+            inputs[0].value.length > 100 ||
+            inputs[1].value.length > 100 ||
+            inputs[2].value == ''
+        ) {
+            document.querySelectorAll('.required').forEach(x => x.setAttribute('class', 'red_outline'))
+            alert('Please fill all fields');
+        } else {
+            const newBook = new Book(inputs[0].value, inputs[1].value, parseInt(inputs[2].value), input3);
+            LibManagement.addBookToLibrary(newBook);
+            displayBooks();
+            form.close();
+        };
+    };
+    addBookBtn.addEventListener('click', addBook);
 
-displayBooks();
+    return {displayBooks};
+})();
+
+LibManagement.addBookToLibrary(TheHobbit);
+LibManagement.addBookToLibrary(TheIdiot);
+Display.displayBooks();
