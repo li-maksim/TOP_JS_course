c7cf505c30498858f5e939637d002cb402a370e7
li-maksim <max.mail4@yandex.ru>
2025-01-20 21:30:55 +0300

    Finished Testing Basics project

--- testing/script.js
+++ testing/script.js
@@ -27,7 +27,61 @@ const calc = function() {
     return {add, sub, mult, divide}
 }()
 
+const ceasarCipher = function(str, num) {
+    const arr = str.split('')
+    for (let i = 0; i < arr.length; i++) {
+        let char = arr[i].charCodeAt()
+        if ((char >= 65 && char <= 90) || (char >= 97 && char <= 122)) {
+            char = char + num
+            if (char > 90 && char < 97) {
+                let diff = char - 91
+                char = 65 + diff
+            } else if (char > 122) {
+                let diff = char - 123
+                char = 97 + diff
+            }
+            arr[i] = String.fromCharCode(char)
+        } else {
+            continue
+        }
+    }
+    return arr.join('')
+}
+
+const arrayFns = function() {
+    const calculateSum = function(total, value) {
+        return total + value
+    }
+    const findMin = function(total, value) {
+        if (total > value) {
+            total = value
+        }
+        return total
+    }
+    const findMax = function(total, value) {
+        if (total < value) {
+            total = value
+        }
+        return total
+    }
+
+    return {calculateSum, findMin, findMax}
+}()
+
+const analyzeArray = function(arr) {
+    const sum = arr.reduce(arrayFns.calculateSum)
+    const obj = {}
+    obj.average = sum / arr.length
+    obj.min = arr.reduce(arrayFns.findMin)
+    obj.max = arr.reduce(arrayFns.findMax)
+    obj.length = arr.length
+
+    return obj
+}
+
 module.exports = {}
 module.exports.capitalize = capitalize
 module.exports.reverseString = reverseString
 module.exports.calc = calc
+module.exports.ceasarCipher = ceasarCipher
+module.exports.analyzeArray = analyzeArray
--- testing/script.test.js
+++ testing/script.test.js
@@ -1,28 +1,63 @@
 const script = require('./script')
 
-test('Capitalizes word', () => {
-    expect(script.capitalize('word')).toBe('WORD')
-})
-test('Capitalizes apple', () => {
-    expect(script.capitalize('applE')).toBe('APPLE')
-})
-// test('Throws an error when argument is not a string', () => {
-//     expect(script.capitalize(1)).toThrow()
+// test('Capitalizes word', () => {
+//     expect(script.capitalize('word')).toBe('WORD')
+// })
+// test('Capitalizes apple', () => {
+//     expect(script.capitalize('applE')).toBe('APPLE')
+// })
+// // test('Throws an error when argument is not a string', () => {
+// //     expect(script.capitalize(1)).toThrow()
+// // })
+
+// test('Reverses "Word"', () => {
+//     expect(script.reverseString('Word')).toBe('dorW')
 // })
 
-test('Reverses "Word"', () => {
-    expect(script.reverseString('Word')).toBe('dorW')
-})
+// test('Adds 2 + 3', () => {
+//     expect(script.calc.add(2, 3)).toBe(5)
+// })
+// test('Subtracts 2 - 3', () => {
+//     expect(script.calc.sub(2, 3)).toBe(-1)
+// })
+// test('Multiplies 2 * 3', () => {
+//     expect(script.calc.mult(2, 3)).toBe(6)
+// })
+// test('Divides 10 / 4', () => {
+//     expect(script.calc.divide(10, 4)).toBe(2.5)
+// })
 
-test('Adds 2 + 3', () => {
-    expect(script.calc.add(2, 3)).toBe(5)
-})
-test('Subtracts 2 - 3', () => {
-    expect(script.calc.sub(2, 3)).toBe(-1)
-})
-test('Multiplies 2 * 3', () => {
-    expect(script.calc.mult(2, 3)).toBe(6)
-})
-test('Divides 10 / 4', () => {
-    expect(script.calc.divide(10, 4)).toBe(2.5)
+// test('Turns "abc" into "def"', () => {
+//     expect(script.ceasarCipher('abc', 3)).toBe('def')
+// })
+
+// test('Turns "ayz" into "dbc"', () => {
+//     expect(script.ceasarCipher('ayz', 3)).toBe('dbc')
+// })
+// test('Turns "bbBX" into "eeEA"', () => {
+//     expect(script.ceasarCipher('bbBX', 3)).toBe('eeEA')
+// })
+// test('Turns "Hello, world!" into "Khoor, Zruog!"', () => {
+//     expect(script.ceasarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+// })
+
+// test('Returns average', () => {
+//     expect(script.analyzeArray([1, 2, 3, 4])).toBe(2.5)
+// })
+// test('Returns min', () => {
+//     expect(script.analyzeArray([0, 2, 3, 1, -4])).toBe(-4)
+// })
+// test('Returns max', () => {
+//     expect(script.analyzeArray([0, 2, 3, 1, -4])).toBe(3)
+// })
+// test('Returns length', () => {
+//     expect(script.analyzeArray([0, 2, 3, 1, -4])).toBe(5)
+// })
+test('Returns object with right properties', () => {
+    expect(script.analyzeArray([0, 2, 3, 1])).toEqual({
+        average: 1.5,
+        min: 0,
+        max: 3,
+        length: 4
+    })
 })
